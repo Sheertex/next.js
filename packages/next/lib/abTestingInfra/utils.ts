519cd9c3a9fc037e5235340eab6a0fb94bce9493
Ilia Pirozhenko <ilia@ipirozhenko.com>
2020-05-01 18:47:03 +0300

    Moved every component for ABTesting to next.js

--- packages/next/lib/abTestingInfra/utils.ts
+++ packages/next/lib/abTestingInfra/utils.ts
@@ -4,6 +4,29 @@ import { ParsedUrlQuery } from 'querystring'
 
 const AB_TEST_PAYLOAD_PREFIX = '--ab--'
 
+function isValidName(name: string): boolean {
+  if (!name) {
+    return false
+  }
+
+  //Name should contain only a-z 0-9 and dash as it will be passed in the url
+  return /^[a-z0-9-]+$/.test(name)
+}
+
+function assertNameIsValid(name: string): void {
+  if (!name) {
+    throw new Error('Name is undefined')
+  }
+
+  if (!isValidName(name)) {
+    throw new Error(
+      `"${name}" is not valid name. Use only lowercase latin symbols, digits and dash`
+    )
+  }
+
+  return
+}
+
 interface Experiment {
   name: string
   variants: Array<string>
@@ -63,7 +86,7 @@ function tryAddABTestingPayload(
   const pathToExperimentsPayload = nodePath.join(CWD, 'experiments.json')
 
   if (!fs.existsSync(pathToExperimentsPayload)) {
-    console.warn('Failed to load experiments payload, file not found')
+    console.log('Failed to load experiments payload, file not found')
 
     return paths
   }
@@ -116,4 +139,49 @@ function tryAddABTestingPayload(
   return result
 }
 
-export { tryAddABTestingPayload }
+function tryStripABTestingPayloadFromQuery(
+  query: ParsedUrlQuery
+): ParsedUrlQuery {
+  if (!query) {
+    return query
+  }
+
+  // console.log(`IN: ${JSON.stringify(query)}`)
+
+  let abTestingPayload = ''
+
+  const resultQuery: ParsedUrlQuery = {}
+
+  Object.keys(query).forEach(key => {
+    const value = query[key]
+    if (typeof value !== 'string') {
+      resultQuery[key] = value
+      return
+    }
+
+    const idx = value.indexOf(AB_TEST_PAYLOAD_PREFIX)
+    if (idx === -1) {
+      resultQuery[key] = value
+      return
+    }
+
+    resultQuery[key] = value.slice(0, idx)
+    abTestingPayload = value.slice(idx + AB_TEST_PAYLOAD_PREFIX.length)
+  })
+
+  if (!!abTestingPayload) {
+    resultQuery['AB_TESTING_PAYLOAD'] = abTestingPayload
+  }
+
+  // console.log(`OUT: ${JSON.stringify(resultQuery)}`)
+
+  return resultQuery
+}
+
+export {
+  AB_TEST_PAYLOAD_PREFIX,
+  tryAddABTestingPayload,
+  tryStripABTestingPayloadFromQuery,
+  assertNameIsValid,
+  isValidName,
+}
