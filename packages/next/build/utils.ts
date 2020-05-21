6b5adcfe24845250bb882159c87f7632c1a0507f
Ilia Pirozhenko <ilia@ipirozhenko.com>
2020-05-21 17:46:09 +0300

    Merge fix

--- packages/next/build/utils.ts
+++ packages/next/build/utils.ts
@@ -769,14 +769,6 @@ export async function isPageStatic(
       prerenderRoutes = await mod.permuteStaticPaths(prerenderRoutes || [page])
     }
 
-    if (hasPermutations) {
-      prerenderRoutes = await mod.permuteStaticPaths(prerenderRoutes || [page])
-    }
-
-    if (hasPermutations) {
-      prerenderRoutes = await mod.permuteStaticPaths(prerenderRoutes || [page])
-    }
-
     const config = mod.config || {}
     return {
       isStatic: !hasStaticProps && !hasGetInitialProps && !hasServerProps,
